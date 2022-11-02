38bdb51a3e18c270db1561c9de0477ca02db3198
diegomorro <55061540+diegomorro@users.noreply.github.com>
2022-11-02 12:48:35 -0400

    2nd commit

--- tests/test_cipher_dm3760.py
+++ tests/test_cipher_dm3760.py
@@ -1,8 +1,7 @@
 from cipher_dm3760 import cipher_dm3760
-import pytest
     
 def test_cipher_function_1a():
     example = 'Hello'
     expected = 'Khoor'
-    actual = cipher(example, 3)
+    actual = cipher_dm3760.cipher(example, 3)
     assert actual == expected
